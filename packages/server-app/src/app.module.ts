cc6f526b249a9b9f930ab814de32adc5240617b2
Keita Ito <eguchi@eguchi.cc>
2024-06-02 15:14:49 +0900

    chore(server): post関連の実装を削除

--- packages/server-app/src/app.module.ts
+++ packages/server-app/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { PrismaService } from "./prisma/prisma.service";
-import { PostModule } from "./post/post.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
-  imports: [PostModule, PrismaModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
