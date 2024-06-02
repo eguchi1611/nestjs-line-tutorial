e6f145c7ba718de08deeca284087868524354c75
Keita Ito <eguchi@eguchi.cc>
2024-06-02 15:17:47 +0900

    chore(server): postsの受け取り部分の作成

--- packages/server-app/src/app.module.ts
+++ packages/server-app/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { PrismaService } from "./prisma/prisma.service";
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PostsModule],
   providers: [PrismaService],
 })
 export class AppModule {}
