10ae399b1acd9786e0e44e5a55e7cd31bb431277
Keita Ito <eguchi@eguchi.cc>
2024-06-03 14:32:30 +0900

    feat(server): usersモジュールの作成

--- packages/server-app/src/app.module.ts
+++ packages/server-app/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { PrismaService } from "./prisma/prisma.service";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   providers: [PrismaService],
 })
 export class AppModule {}
