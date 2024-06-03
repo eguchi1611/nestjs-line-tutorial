cbb4c03ab729ed9b1d8e38f99053424b2bc7bba0
Keita Ito <eguchi@eguchi.cc>
2024-06-04 05:43:21 +0900

    feat(server): サーバーパスを変更

--- packages/server-app/src/main.ts
+++ packages/server-app/src/main.ts
@@ -1,11 +1,13 @@
-import { HttpAdapterHost, NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { PrismaClientExceptionFilter } from "./prisma-client-exception/prisma-client-exception.filter";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import { PrismaClientExceptionFilter } from "./prisma-client-exception/prisma-client-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("v1");
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, validateCustomDecorators: true }),
@@ -16,10 +18,13 @@ async function bootstrap() {
     .setDescription("nestjs-line-tutorial API description")
     .setVersion("0.1")
     .addBearerAuth()
+    .addServer("/v1")
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true,
+  });
+  SwaggerModule.setup("docs", app, document);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
