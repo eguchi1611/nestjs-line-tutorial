7692d3a379b0df00e1cca1f97f50c417362a9c99
Keita Ito <eguchi@eguchi.cc>
2024-06-03 14:57:05 +0900

    feat(server): usersモジュールの実装

--- packages/server-app/src/users/dto/create-user.dto.ts
+++ packages/server-app/src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  lineUid: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiProperty()
+  comment?: string;
+}
--- packages/server-app/src/users/entities/user.entity.ts
+++ packages/server-app/src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  lineUid: string;
+
+  @ApiProperty({ nullable: true })
+  comment: string | null;
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+}
