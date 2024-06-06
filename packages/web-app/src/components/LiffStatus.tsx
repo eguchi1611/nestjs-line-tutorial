678f2d557a3cb2e5869d68d0e7149004b6ce325b
Keita Ito <eguchi@eguchi.cc>
2024-06-07 04:23:34 +0900

    feat(web): バックエンドとのAPI通信を実装

--- packages/web-app/src/components/LiffStatus.tsx
+++ packages/web-app/src/components/LiffStatus.tsx
@@ -1,27 +1,34 @@
 "use client";
 
-import { UsersApi } from "@/__generated";
+import { Configuration, UsersApi } from "@/__generated";
 import { useLiff } from "@/features/liff/use-liff";
 import { useEffect, useState } from "react";
 
 export function LiffStatus() {
   const { liff } = useLiff();
   const [token, setToken] = useState<string | null>(null);
+  const [token2, setToken2] = useState<{} | null>(null);
 
   useEffect(() => {
     if (liff) {
       setToken(liff.getAccessToken());
       (async () => {
-        const a = new UsersApi();
+        const a = new UsersApi(
+          new Configuration({
+            accessToken: liff.getAccessToken() || "",
+          }),
+          "http://localhost:8000/v1",
+        );
         const b = await a.findAll();
-        b.data[0].comment;
+        setToken2(b.data);
+        a.update(8, { comment: new Date().toLocaleString() });
       })();
     }
   }, [liff]);
 
   return (
     <div>
-      <pre>{JSON.stringify({ liff, token }, null, 2)}</pre>
+      <pre>{JSON.stringify({ liff, token, token2 }, null, 2)}</pre>
     </div>
   );
 }
