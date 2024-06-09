30022ba7110e7a142bcb12b5c030790190515f29
Keita Ito <eguchi@eguchi.cc>
2024-06-09 23:58:48 +0900

    feat: デプロイを構成

--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
-import { User } from "@prisma/client";
 
 @Injectable()
 export class UsersService {
